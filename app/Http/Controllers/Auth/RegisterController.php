b06acdb8a624b97648e8b6344b9ab26cab28c76d
musyahya <mananabdul37654@gmail.com>
2021-07-24 11:23:06 +0700

    register sesuai role

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -70,6 +70,6 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-        ]);
+        ])->assignRole($data['role']);
     }
 }
