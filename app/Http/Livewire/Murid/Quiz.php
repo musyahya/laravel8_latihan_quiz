7ae1b708657125a57d377658c3f70a300c3cc22b
musyahya <mananabdul37654@gmail.com>
2021-07-27 18:49:01 +0700

    search quiz murid

--- app/Http/Livewire/Murid/Quiz.php
+++ app/Http/Livewire/Murid/Quiz.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Livewire\Murid;
 
-use App\Models\Quiz as ModelsQuiz;
-use App\Models\User;
 use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 use Livewire\WithPagination;
@@ -13,7 +11,7 @@ class Quiz extends Component
     use WithPagination;
     protected $paginationTheme = 'bootstrap';
 
-    public $soal;
+    public $soal, $search;
 
     public function soal($id)
     {
@@ -38,11 +36,20 @@ class Quiz extends Component
 
     public function render()
     {
-        $quiz = DB::table('quiz_murid')
-            ->join('quiz', 'quiz_murid.quiz_id', '=', 'quiz.id')
-            ->where('murid_id', auth()->id())
-            ->select('quiz.nama', 'quiz_murid.*')
-            ->paginate(5);
+        if ($this->search) {
+            $quiz = DB::table('quiz_murid')
+                ->join('quiz', 'quiz_murid.quiz_id', '=', 'quiz.id')
+                ->where('murid_id', auth()->id())
+                ->where('quiz.nama', 'like', '%'. $this->search .'%')
+                ->select('quiz.nama', 'quiz_murid.*')
+                ->paginate(5);
+        } else {
+            $quiz = DB::table('quiz_murid')
+                ->join('quiz', 'quiz_murid.quiz_id', '=', 'quiz.id')
+                ->where('murid_id', auth()->id())
+                ->select('quiz.nama', 'quiz_murid.*')
+                ->paginate(5);
+        }
 
         return view('livewire.murid.quiz', compact('quiz'));
     }
