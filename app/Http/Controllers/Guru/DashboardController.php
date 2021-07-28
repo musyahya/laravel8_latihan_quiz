43faac8e64a19342a668ce1cb9fd751349b1f93f
musyahya <mananabdul37654@gmail.com>
2021-07-28 11:38:35 +0700

    dahsboard guru

--- app/Http/Controllers/Guru/DashboardController.php
+++ app/Http/Controllers/Guru/DashboardController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Guru;
 
 use App\Http\Controllers\Controller;
+use App\Models\Quiz;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -15,6 +17,12 @@ class DashboardController extends Controller
      */
     public function __invoke(Request $request)
     {
-        return view('guru/dashboard');
+        $murid = User::role('murid')->count();
+        $guru = User::role('guru')->count();
+        $quiz = Quiz::where('guru_id', auth()->id())->count();
+
+        $quiz_terbaru = Quiz::where('guru_id', auth()->id())->limit(5)->latest()->get();
+        $murid_terbaru = User::role('murid')->limit(5)->latest()->get();
+        return view('guru/dashboard', compact('guru', 'murid', 'quiz', 'quiz_terbaru', 'murid_terbaru'));
     }
 }
